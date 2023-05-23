5b3d3c846d0314fab18c725a624cf84043eb374f
Thiago Alves <thiagosales06@hotmail.com>
2023-05-23 10:55:26 -0300

    Criação do frontend

---
+++ ControleDeFuncionarios/Controllers/Cadastro.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeFuncionarios.Controllers
+{
+    public class Cadastro : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ControleDeFuncionarios/Controllers/Colaborador.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeFuncionarios.Controllers
+{
+    public class Colaborador : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }        
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Visualizar()
+        {
+            return View();
+        }
+    }
+}
